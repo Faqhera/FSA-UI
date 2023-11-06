488ceebce979ef1724b6903c73624aa0366b430d
Faqhera <faqhera2@gmail.com>
2023-11-06 07:39:31 +0530

    Redesigning user component

--- src/components/Lang.jsx
+++ src/components/Lang.jsx
@@ -1,16 +1,15 @@
+import styles from "./Lang.module.css"
 const Lang = ({ln}) => {
     //const cssStyle = {
         //color:"black",
        // paddingTop: "8px",
       //  backgroundColor: "pink",
     //};
-return (
     //<p style = {cssStyle} className="bg-blue-700 p-3 m-3 border-2 border-red-600">
-    <p className="bg-blue-700 p-3 m-3 border-2 border-red-600">
+    //return ( <p style={{fontSize: "25px"}} className="bg-blue-700 p-3 m-3 border-2 border-red-600">{ln}</p> );
+   
+    return <p className={styles.p}>{ln}</p>
 
-        {ln}
-    </p>
-);
 };
 
 
